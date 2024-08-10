17433384a8869aa3a573020b347b2170b52cb17c
murad khan <you@example.com>
2024-08-10 19:33:22 +0500

    java script class 1

---
+++ app.js
@@ -0,0 +1,20 @@
+const account_ID =1235
+let account_email ="murad@gamil.com"
+var account_password ="12346"
+ Account_city = "karachi"
+
+
+//  account_ID = "3"// not allowed //
+account_email ="khan@.com"
+account_password = "125365"
+Account_city = "islamabad"
+
+ console.log(account_ID);
+  
+ console.table ([account_ID,account_email,account_password,Account_city])
+
+
+ /*
+prefere not use var
+beacuse of issues block scope  and functional scope
+ */
